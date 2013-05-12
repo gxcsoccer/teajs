e7bb60744d2d471bff0e52b21fff17ba9e078fcd
gaoxiaochen <gxcsoccer@126.com>
2013-05-13 05:40:03 +0800

    add method

--- src/util/Utils.js
+++ src/util/Utils.js
@@ -24,16 +24,37 @@ define(function(require, exports, module) {
 
 	Utils.mix = function(r, s, wl) {
 		// Copy "all" properties including inherited ones.
-		for(var p in s) {
-			if(s.hasOwnProperty(p)) {
-				if(wl && indexOf(wl, p) === -1)
-					continue;
+		for (var p in s) {
+			if (s.hasOwnProperty(p)) {
+				if (wl && indexOf(wl, p) === -1) continue;
 
 				// 在 iPhone 1 代等设备的 Safari 中，prototype 也会被枚举出来，需排除
-				if(p !== 'prototype') {
+				if (p !== 'prototype') {
 					r[p] = s[p];
 				}
 			}
 		}
-	}
+	};
+
+	/**
+	 * 不严谨的深拷贝
+	 */
+	Utils.clone = function(input) {
+		if (!Utils.isObject(input)) {
+			return input;
+		}
+
+		var type = Object.prototype.toString.call(input);
+		switch (type) {
+		case "[object Object]":
+			return Object.create(input, JSON.parse(JSON.stringify(input)));
+		case "[object Array]":
+			return input.map(function(item) {
+				return Object.create(item, JSON.parse(JSON.stringify(item)));
+			});
+			break;
+		default:
+			return input;
+		}
+	};
 });
