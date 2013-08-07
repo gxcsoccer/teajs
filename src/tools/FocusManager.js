abf21baace1afcfbf4edbcd969d1245d025d0cd4
gaoxiaochen <gxcsoccer@126.com>
2013-08-07 23:33:30 +0800

    update

---
+++ src/tools/FocusManager.js
@@ -0,0 +1,22 @@
+define(function(require, exports, module) {
+	'use strict';
+	var Class = require('../core/class/Class');
+
+	return Class.extend({
+		init: function() {
+
+		},
+		left: function() {
+
+		},
+		right: function() {
+
+		},
+		up: function() {
+
+		},
+		down: function() {
+
+		}
+	})
+});
