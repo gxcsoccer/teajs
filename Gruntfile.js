111625f821b38a3dbe9dc54f458d5e39946066c8
gaoxiaochen <gxcsoccer@126.com>
2013-08-20 23:31:00 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt) {
-    
+
     // 项目配置信息.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -11,12 +11,25 @@ module.exports = function(grunt) {
                 src: 'src/<%= pkg.name %>.js',
                 dest: 'build/<%= pkg.name %>.min.js'
             }
+        },
+        jshint: {
+            //all: ['src/*.js'],
+            options: {
+                curly: true,
+                eqeqeq: true,
+                eqnull: true,
+                browser: true,
+                globals: {
+                    jQuery: true
+                }
+            }
         }
     });
 
     // 加载"uglify"插件..
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
     // 注册默认任务.
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['jshint', 'uglify']);
 };
